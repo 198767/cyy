f5a6b886b7279e21911c7b070ee1fa2394e12b2f
unknown <Owner@lenovo-cf754a41.(none)>
2011-10-16 19:46:14 +0800

    euler

--- euler/euler16.c
+++ euler/euler16.c
@@ -1,39 +1,14 @@
 #include<stdio.h>
-#define N 4
-int n[N][N]=
-{
-{3},
-{7,4},
-{2,4,6},
-{8,5,9,3}
-};
+#include "ln.h"
 int main()
 {
-	int i,j;
-	int max=0;
-	for(i=1;i<N;i++)
+	lN a=str2ln(NULL,"2");
+	if(!a)
 	{
-		printf("line %d:",i);
-		for(j=0;j<=i;j++) 
-			printf("%d ",n[i][j]);
-		printf("\n");
+		printf("创建失败");
+		return -1;
 	}
-	for(i=1;i<N;i++)
-	{
-		n[i][0]+=n[i-1][0];
-		n[i][i]+=n[i-1][i-1];
-		for(j=1;j<i;j++) 
-			n[i][j]+=(n[i-1][j-1]>n[i-1][j]?n[i-1][j-1]:n[i-1][j]);
-	}
-
-
-	for(i=1;i<N;i++)
-	{
-		printf("line %d:",i);
-		for(j=0;j<=i;j++) 
-			printf("%d ",n[i][j]);
-		printf("\n");
-	}
-
+	a=ln_exp_int(a,1000,firstln);
+	printf("%d",ln_digitsum(a));
 	return 0;
 }
