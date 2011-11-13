06f8fbd707b3002e14713344330af70dd0749be7
cyy <cyy@cyy-laptop.(none)>
2011-11-13 15:36:17 +0800

    ln

--- ln/ln_arithmetic.c
+++ ln/ln_arithmetic.c
@@ -319,7 +319,7 @@ ln ln_multiply(ln i,ln j,res_type restype)
 		return  k;
 	else
 	{
-		ln_copy(i,k);
+		i=ln_copy(i,k);
 		ln_free(&k);
 		return i;
 	}
@@ -469,12 +469,7 @@ ln ln_divide_int(ln a,int b,int precision,divide_mode mode,res_type restype)
 	{
 		res+=carry*UNIT;
 		z->num=res/b;
-		//carry=res%b;
-		carry=res-z->num*b;
-printf("res=%d carry=%d\n",res,carry);
-printf("  b=%d\n",b);
-printf("inc_prec=%d num=%d\n",inc_prec,z->num);
-
+		carry=res%b;
 		//开始计算当前精度
 		inc_prec+=DIGIT_NUM;
 		if(inc_prec>precision) //已经达到需要的精度
@@ -485,8 +480,8 @@ printf("inc_prec=%d num=%d\n",inc_prec,z->num);
 			c->power=-inc_prec;
 			break;
 		}
-
-		if(x==a->lsd) //必须这样标识，否则如果a和c一样，则下面会修改lsd
+		//必须这样标识，否则如果a和c一样，则下面会修改lsd
+		if(x==a->lsd)
 			x=NULL;
 		if(x==NULL)
 		{
@@ -517,214 +512,139 @@ printf("inc_prec=%d num=%d\n",inc_prec,z->num);
 	}
 
 	//获取精度
-	puts("处理之前");
-	ln_output(c);
 	ln_fix(c,precision,mode);
 	return c;
 }
-
 #ifdef dsa
-ln ln_divide_int(ln i,int b,int precision,Divide_mode mode,res_type restype)
+
+/*
+ * 作用:把ln相除
+ * 参数:
+ * 	a:除数
+ * 	b:被除数
+ *	precision:所需精度(保留的小数位数)
+ *	mode:指定截断或者四舍五入
+ *	restype:结果存放方式
+ * 返回值:
+ * 	成功:返回相加结果
+ * 	失败:NULL
+ */
+ln ln_divide(ln a,ln b)//,int precision,Divide_mode mode),res_type restype)
 {
-	int res=0;
-	int carry=0;
-	int prec=0;
-	ln k;
-	assert(b!=0);
-	assert(precision>=0);
-	if(precision<0)
-		precision=DIV_PREC;
+	ln x,y,z;
+	int zero;
+	int a_digitnum;
+	int b_digitnum;
+	int flag=0;
+	cell x,y,z;
+	int o;
 
-	if(restype==newln)
+	//验证参数
+	if(ln_checknull(a)!=0)
 	{
-		if(b==1|| b==-1)
-		{
-			k= copy_lns(NULL,i);
-			return ln_multiply_num(k,b,firstln);
-		}
-		k=ln_creat(ln_get_nodenum(i,i->msd));
-		k->power=i->power;
-		if(b>0)
-			k->sign=i->sign;
-		else
-		{
-			k->sign=!(i->sign);
-			b=-b;
-		}
-		cell a=i->msd;
-		while(a!= i->lsd && a->num==0)
-			a=a->lcell;
-		i->msd=a;
-		k->msd=k->lsd->lcell;
-		cell c=k->msd;
-		res=a->num;
-		while(1)
-		{
-			res=res+carry*UNIT;
-			c->num=res/b;
-			carry=res%b;
-			if(k->power<0)
-			{
-				if(prec==0)
-				{
-					if(c->num !=0) //开始计算小数点个数
-						prec=-k->power;
-				}
-				else
-					prec+=DIGIT_NUM;
-			}
-			if(a==i->lsd)
-			{
-				if(carry==0) //除得尽
-				{
-					k->lsd=c;
-					break;
-				}
-				else if(prec>=precision) //除不尽，四舍五入保留precision位小数
-				{
-					if(prec==precision)
-					{
-						if(mode==round)
-						{
-							if(a !=i->lsd)
-								res=(a->lcell->num+carry*UNIT)/b;
-							else
-								res=(carry*UNIT)/b;
-							if(res>=UNIT/2)
-								c->num++;
-						}
-					}
-					else
-					{
-						int l,j=1;
-						for(l=0;l<prec-precision;l++)
-							j*=10;
-						res=c->num%j;
-						if(mode==round && res>=5*(j/10))
-							c->num=c->num-res+j;
-						else
-							c->num=c->num-res;
-					}
-					k->lsd=c;
-					break;
-				}
-			}
-			if(a !=i->lsd)
-			{
-				a=a->lcell;
-				res=a->num;
-			}
-			else
-			{
-				res=0;
-				k->power-=DIGIT_NUM;
-			}
-			if(c==k->lsd)
-			{
-				ln_addsize(k,5);
-				k->lsd=k->msd->hcell;
-			}
-			c=c->lcell;
-		}
-		return k;
+		fprintf(stderr,"[%s %d] %s error,reason: ln_checknull fail\n",__FILE__,__LINE__,__FUNCTION__);
+		return NULL;	
 	}
+	if(ln_checknull(b)!=0)
+	{
+		fprintf(stderr,"[%s %d] %s error,reason: ln_checknull fail\n",__FILE__,__LINE__,__FUNCTION__);
+		return NULL;	
+	}
+
+	//复制ln
+	x=ln_copy(x,a);
+	
+
+
+	y=ln_copy(y,b);
+	c=init_ln(0);
+
+	if(a->sign==b->sign)
+		c->sign=1;
 	else
+		c->sign=-1;
+	a_digitnum=ln_nodenum(a);
+	b_digitnum=ln_nodenum(b);
+	if(a_digitnum<b_digitnum)
 	{
-		if(b==1|| b==-1 )
-		{
-			return ln_multiply_num(i,b,firstln);
-		}
-		if(b<0)
-		{
-			i->sign=!(i->sign);
-			b=-b;
-		}
-		cell a=i->msd;
-		while(a != i->lsd && a->num==0)
-			a=a->lcell;
-		cell c=i->lsd->lcell;
-		cell d=i->lsd;
-		i->msd=c;
-		res=a->num;
-		while(1)
-		{
-			res=res+carry*UNIT;
-			c->num=res/b;
-			carry=res%b;
-			if(i->power<0)
-			{
-				if(prec==0)
-				{
-					if(c->num !=0) //开始计算小数点个数
-						prec=-i->power;
-				}
-				else
-					prec+=DIGIT_NUM;
-			}
-			if(a==d)
-			{
-				if(carry==0) //除得尽
-				{
-					i->lsd=c;
-					break;
-				}
-				else if(prec>=precision) //除不尽，四舍五入保留precision位小数
-				{
-					if(prec==precision)
-					{
-						if(mode==round)
-						{
-							if(a !=d)
-								res=(a->lcell->num+carry*UNIT)/b;
-							else
-								res=(carry*UNIT)/b;
-							if(res>=UNIT/2)
-								c->num++;
-						}
-					}
-					else
-					{
-						int l,j=1;
-						for(l=0;l<prec-precision;l++)
-							j*=10;
-						res=c->num%j;
-						if(mode==round && res>=5*(j/10))
-							c->num=c->num-res+j;
-						else
-							c->num=c->num-res;
-					}
-					i->lsd=c;
-					break;
-				}
-			}
-			if(a!=d)
-			{
-				a=a->lcell;
-				res=a->num;
-			}
-			else
-			{
-				res=0;
-				i->power-=DIGIT_NUM;
-			}
-			if(c==i->lsd)
-			{
-				ln_addsize(i,5);
-				i->lsd=i->msd->hcell;
-			}
-			c=c->lcell;
-		}
-		return i;
+		zero=a->power-(b_digitnum-a_digitnum)*DIGIT_NUM;
+		ln_setzero(a,zero);
 	}
+	else if(a_digitnum>b_digitnum)
+	{
+		zero=b->power-(a_digitnum-b_digitnum)*DIGIT_NUM;
+		ln_setzero(b,zero);
+	}
+	zero=a->power-b->power;
+
+	a->sign=1;
+	a->power=0;
+	b->power=0;
+	c->power=0;
+
+	x=a->msd;
+	y=b->msd;
+	z=c->msd;
+
+	for(o=1;o<4;o++)
+	{
+
+		if(x!=a->msd) 
+			z->num=(x->num+x->hcell->num*UNIT)/y->num; //求出z位数
+		else
+			z->num=x->num/y->num; //求出z位数
+		if(z->num !=0)
+		{
+			b->sign=-1; //b变成负数
+			q=ln_multiply_num(b,z->num,newln);
+			ln_add(a,q,firstln);
+			ln_free(q);
+			while(a->sign==1) //少减了，再减去
+			{
+				b->sign=-1;
+				ln_add(a,b,firstln);
+				z->num++;
+			}
+			while(a->sign==-1) //多减了，补上
+			{
+				b->sign=1;
+				ln_add(a,b,firstln);
+				z->num--;
+			}
+		}
+
+		c->lsd=z;
+		if(flag==1)
+			zero-=DIGIT_NUM;
+		flag=1;
+		if(ln_cmp_num(a,0)==0)
+			break;
+
+		if(a->msd==a->lsd->lcell)
+		{
+			ln_addsize(a,INIT_SIZE);
+			puts("aaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+		}
+		if(a->msd->num==0)
+			a->msd=a->msd->lcell;
+		a->lsd=a->lsd->lcell;
+		a->lsd->num=0;
+		x=x->lcell;
+
+		puts(ln2str(a));
+
+		if(c->lsd->lcell==c->msd)
+			ln_addsize(c,INIT_SIZE);
+		z=z->lcell;
+
+	}
+	while(c->msd!=c->lsd && c->msd->num==0)
+		c->msd=c->msd->lcell;
+	c->power=zero;
+	ln_free(a);
+	ln_free(b);
+	return c;
 }
-
-
-
-
-
-
-
-
 
 
 
@@ -788,11 +708,6 @@ ln ln_exp_int(ln i,int b,res_type restype)
 		}
 	}
 }
-
-
-
-
-
 
 int ln_divideable_num(const ln i,int b)
 {
@@ -888,110 +803,6 @@ ln ln_square(ln a,res_type restype)
 	}
 }
 
-/*
- * 大数和大数想除
- */
-
-ln ln_divide_ln(ln i,ln j)//,int precision,Divide_mode mode),res_type restype)
-{
-	ln a,b,c,q;
-	int zero;
-	int a_digitnum;
-	int b_digitnum;
-	int flag=0;
-	cell x,y,z;
-	int o;
-
-	a=copy_lns(NULL,i);
-	b=copy_lns(NULL,j);
-	c=init_ln(0);
-
-	if(a->sign==b->sign)
-		c->sign=1;
-	else
-		c->sign=-1;
-	a_digitnum=ln_nodenum(a);
-	b_digitnum=ln_nodenum(b);
-	if(a_digitnum<b_digitnum)
-	{
-		zero=a->power-(b_digitnum-a_digitnum)*DIGIT_NUM;
-		ln_setzero(a,zero);
-	}
-	else if(a_digitnum>b_digitnum)
-	{
-		zero=b->power-(a_digitnum-b_digitnum)*DIGIT_NUM;
-		ln_setzero(b,zero);
-	}
-	zero=a->power-b->power;
-
-	a->sign=1;
-	a->power=0;
-	b->power=0;
-	c->power=0;
-
-	x=a->msd;
-	y=b->msd;
-	z=c->msd;
-
-	for(o=1;o<4;o++)
-	{
-
-		if(x!=a->msd) 
-			z->num=(x->num+x->hcell->num*UNIT)/y->num; //求出z位数
-		else
-			z->num=x->num/y->num; //求出z位数
-		if(z->num !=0)
-		{
-			b->sign=-1; //b变成负数
-			q=ln_multiply_num(b,z->num,newln);
-			ln_add(a,q,firstln);
-			ln_free(q);
-			while(a->sign==1) //少减了，再减去
-			{
-				b->sign=-1;
-				ln_add(a,b,firstln);
-				z->num++;
-			}
-			while(a->sign==-1) //多减了，补上
-			{
-				b->sign=1;
-				ln_add(a,b,firstln);
-				z->num--;
-			}
-		}
-
-		c->lsd=z;
-		if(flag==1)
-			zero-=DIGIT_NUM;
-		flag=1;
-		if(ln_cmp_num(a,0)==0)
-			break;
-
-		if(a->msd==a->lsd->lcell)
-		{
-			ln_addsize(a,INIT_SIZE);
-			puts("aaaaaaaaaaaaaaaaaaaaaaaaaaaa");
-		}
-		if(a->msd->num==0)
-			a->msd=a->msd->lcell;
-		a->lsd=a->lsd->lcell;
-		a->lsd->num=0;
-		x=x->lcell;
-
-		puts(ln2str(a));
-
-		if(c->lsd->lcell==c->msd)
-			ln_addsize(c,INIT_SIZE);
-		z=z->lcell;
-
-	}
-	while(c->msd!=c->lsd && c->msd->num==0)
-		c->msd=c->msd->lcell;
-	c->power=zero;
-	ln_free(a);
-	ln_free(b);
-	return c;
-}
 
 
 //获取ln的十进制位数的和
